7474ba96f15d361bfb14aa0908bac6239c3bb76d
Vitalii <svitalii138@gmail.com>
2023-06-24 22:49:46 +0300

    Start

---
+++ app.js
@@ -0,0 +1,12 @@
+// BASIC
+var age;
+age = 50;
+var nameString;
+nameString = 'Max';
+var toggle;
+toggle = true;
+var empty;
+empty = null;
+var notInitialize;
+notInitialize = undefined;
+var callback = function (a) { return 100 + a; };
---
+++ app.ts
@@ -0,0 +1,22 @@
+// BASIC
+let age: number;
+let nameString: string;
+let toggle: boolean;
+let empty: null;
+let notInitialize: undefined;
+let callback = (a): number => {
+  return 100 + a;
+};
+
+age = 50;
+nameString = "Max";
+toggle = true;
+empty = null;
+notInitialize = undefined;
+
+// ANY
+let anything: any;
+anything = -20;
+anything = 'Text';
+anything = {};
+
