930a3dbcd404f4f971803b491278fae7ebf2db91
Vitalii <svitalii138@gmail.com>
2023-07-26 23:57:52 +0300

    Done

--- app.js
+++ app.js
@@ -1,12 +1,77 @@
 // BASIC
 var age;
-age = 50;
 var nameString;
-nameString = 'Max';
 var toggle;
-toggle = true;
 var empty;
-empty = null;
 var notInitialize;
+// let callback = (a: number) => number;
+// callback = a => {
+//   return 100 + a;
+// };
+age = 50;
+nameString = "Max";
+toggle = true;
+empty = null;
 notInitialize = undefined;
-var callback = function (a) { return 100 + a; };
+// ANY
+var anything;
+anything = -20;
+anything = 'Text';
+anything = {};
+// UNKNOWN
+var some;
+some = 'Text';
+var str;
+if (typeof some === 'string') {
+    str = some;
+}
+// TUPPLE
+var person;
+person = ['Max', 21];
+// ENUM
+var Status;
+(function (Status) {
+    Status[Status["LOADING"] = 0] = "LOADING";
+    Status[Status["READY"] = 1] = "READY";
+})(Status || (Status = {}));
+;
+var webpage = { status: Status.READY };
+if (webpage.status === Status.LOADING) {
+    console.log('Status:', Status.LOADING);
+}
+if (webpage.status === Status.READY) {
+    console.log('Status:', Status.READY);
+}
+// UNION
+var a;
+a = 'string';
+a = 122;
+// LITERAL
+var c;
+c = 'disable';
+// FUNCTION TYPE
+function showMessage(message) {
+    console.log(message);
+}
+function calc(num1, num2) {
+    return num1 + num2;
+}
+function customError() {
+    throw new Error('Error');
+}
+var page1 = {
+    title: 'The awesome page',
+    likes: 100,
+    accounts: ['Max', 'Anton', 'Nikita'],
+    status: 'open',
+    details: {
+        createAt: new Date('2021-01-01'),
+        updateAt: new Date('2021-05-01'),
+    }
+};
+var page2 = {
+    title: 'Python or Js',
+    likes: 5,
+    accounts: ['Alex'],
+    status: 'close',
+};
--- app.ts
+++ app.ts
@@ -4,9 +4,11 @@ let nameString: string;
 let toggle: boolean;
 let empty: null;
 let notInitialize: undefined;
-let callback = (a): number => {
-  return 100 + a;
-};
+// let callback = (a: number) => number;
+
+// callback = a => {
+//   return 100 + a;
+// };
 
 age = 50;
 nameString = "Max";
@@ -19,4 +21,90 @@ let anything: any;
 anything = -20;
 anything = 'Text';
 anything = {};
+
+// UNKNOWN
+
+let some:unknown;
+some = 'Text';
+
+let str: string;
+
+if (typeof some === 'string') {
+  str = some;
+}
+
+// TUPPLE
+let person: [string, number]
+person = ['Max', 21];
+  
+// ENUM
+enum Status { LOADING, READY };
+
+const webpage = { status: Status.READY };
+
+if (webpage.status === Status.LOADING) {
+  console.log('Status:', Status.LOADING)
+}
+
+if (webpage.status === Status.READY) {
+  console.log('Status:', Status.READY)
+}
+
+// UNION
+
+let a: string | number;
+a = 'string';
+a = 122;
+
+// LITERAL
+
+let c: 'enable' | 'disable';
+c = 'disable';
+
+// FUNCTION TYPE
+
+function showMessage(message: string): void {
+  console.log(message);
+}
+
+
+function calc(num1: number, num2: number): number {
+  return num1 + num2;
+}
+
+function customError(): never {
+  throw new Error('Error');
+}
+
+
+// CUSTOM TYPE
+
+type page = {
+  title: string,
+  likes: number,
+  accounts: string[],
+  status: string;
+  details?: {
+    createAt: Date;
+    updateAt: Date;
+  }
+}
+
+const page1: page = {
+  title: 'The awesome page',
+  likes: 100,
+  accounts: ['Max', 'Anton', 'Nikita'],
+  status: 'open',
+  details: {
+    createAt: new Date('2021-01-01'),
+    updateAt: new Date('2021-05-01'),
+  }
+}
+
+const page2: page = {
+  title: 'Python or Js',
+  likes: 5,
+  accounts: ['Alex'],
+  status: 'close',
+}
 
